1174e86ff47646977d0f31dea8073a9f13b7f4a7
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-25 00:23:53 +0530

    initial commit

---
+++ src/main/java/com/week7/TestingApp/TestingAppApplication.java
@@ -0,0 +1,13 @@
+package com.week7.TestingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingAppApplication.class, args);
+	}
+
+}
